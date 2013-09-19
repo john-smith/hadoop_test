fed5ebe8ff43fad2577cd882ac4fa6399a8e544e
Yuya OZAWA <yozawa@yahoo-corp.jp>
2013-09-19 20:04:18 +0900

    4.6.3

---
+++ src/main/java/com/neetomo/mahout_in_action/Recommender4_6_3.java
@@ -0,0 +1,54 @@
+package com.neetomo.mahout_in_action;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.common.Weighting;
+import org.apache.mahout.cf.taste.eval.IRStatistics;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.eval.RecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.GenericRecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.slopeone.SlopeOneRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.mahout.common.RandomUtils;
+import org.apache.mahout.cf.taste.impl.recommender.ClusterSimilarity;
+import org.apache.mahout.cf.taste.impl.recommender.FarthestNeighborClusterSimilarity;
+import org.apache.mahout.cf.taste.impl.recommender.TreeClusteringRecommender;
+
+import java.util.List;
+import java.io.File;
+
+
+public class Recommender4_6_3 {
+    public static void main(String[] args) throws Exception {
+        RandomUtils.useTestSeed();
+        DataModel model = new FileDataModel(new File(args[0]));
+        RecommenderBuilder builder = new RecommenderBuilder() {
+                @Override
+                public Recommender buildRecommender(DataModel model) throws TasteException {
+                    UserSimilarity similarity = new LogLikelihoodSimilarity(model);
+                    ClusterSimilarity cSimilarity = new FarthestNeighborClusterSimilarity(similarity);
+                    return new TreeClusteringRecommender(model, cSimilarity, 10);
+                }
+            };
+
+        Recommender recommender = builder.buildRecommender(model);
+        List<RecommendedItem> recommendations = recommender.recommend(1, 5);
+        for(RecommendedItem item : recommendations) {
+            System.out.println("recommend : " + item);
+        }
+
+
+        RecommenderEvaluator absEvaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+        double score = absEvaluator.evaluate(builder, null, model, 0.95, 0.01);
+        System.out.println("score : " + score);
+    }
+}
