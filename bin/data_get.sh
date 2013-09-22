7be5b08c2a0985d3771447ad4f42d43180e3006c
Yuya OZAWA <yozawa@yahoo-corp.jp>
2013-09-22 10:04:32 +0900

    データ周り整理

---
+++ bin/data_get.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+wget http://www.grouplens.org/system/files/ml-100k.zip -N -O src/main/resources/mahout/ml-100k.zip
+wget http://www.grouplens.org/system/files/ml-1m.zip -O src/main/resources/mahout/ml-1m.zip
+wget http://www.occamslab.com/petricek/data/libimseti-complete.zip -O src/main/resources/mahout/libimseti-complete.zip
+
